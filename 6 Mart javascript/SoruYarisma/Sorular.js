1a79e88a1ef47a579f4b793a23ed807395a237db
YoncaCoskun <yoncacoskun92@gmail.com>
2015-03-23 23:39:00 +0200

    Kurs Dokumanları

---
+++ 6 Mart javascript/SoruYarisma/Sorular.js
@@ -0,0 +1,35 @@
+﻿var sorular = [
+    {
+        soruMetni: "Aşağıdakilerden hangisi ile değişken tanımlanır?",
+        cevapA: "vars = deger",
+        cevapB: "var s = deger",
+        cevapC: "s var = deger",
+        cevapD: "@s deger = var",
+        dogruCevap:"b"
+    },
+    {
+        soruMetni: "32.000 değerini bir değişkende tutmalıyım. Bunun için hangisi uygundur?",
+        cevapA: "tinyint",
+        cevapB: "smallint",
+        cevapC: "bigint",
+        cevapD: "int",
+        dogruCevap: "b"
+    },
+    {
+        soruMetni: "Projemdeki dosyaları hangi pencereden takip edebilirim.",
+        cevapA: "Object Explorer",
+        cevapB: "Team Explorer",
+        cevapC: "Solution Explorer",
+        cevapD: "Server Explorer",
+        dogruCevap: "c"
+    },
+    {
+        soruMetni: "Aşağıdakilerden hangisi DDL kodu değildir?",
+        cevapA: "update",
+        cevapB: "alter",
+        cevapC: "create",
+        cevapD: "drop",
+        dogruCevap: "a"
+    }
+
+];
---
+++ AtesAcmaNesneler.js
@@ -0,0 +1,15 @@
+﻿
+function Asker() { // class ların ılk ısmı herzaman buyuk olmalı
+    this.AskerAdi; 
+    this.Silahi;
+
+    this.atesEt = function () {
+        var metin = this.AskerAdi + "\n";
+
+        for (var i = 0; i < 20; i++) {
+            metin += "\t" + this.Silahi + "_" + i + "\n";
+        }
+        return metin;
+    }
+
+}
